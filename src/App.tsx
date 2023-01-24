7a8d85e22999f00c89fbc3a5339a14c5308268de
ryo-is <ryosuke.izumi62@gmail.com>
2023-01-24 11:34:37 +0900

    feat: ajust style

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,11 @@ import { increment, decrement } from './stores/redux/count/slice';
 import { useCountValue, useCountSetValue } from './contexts/count';
 import {
   countAtom,
+  count2Atom,
   getCountValue,
+  getCount2Value,
   getDoubleCountValue,
+  getSumCountValue,
   countAtomWithStorageAtom,
 } from './stores/jotai/atom';
 
@@ -37,149 +40,190 @@ export const App = () => {
   const setCountByContext = useCountSetValue();
 
   // jotai
-  const [countByJotai, setCountByJotai] = useAtom(countAtom);
+  const [, setCountByJotai] = useAtom(countAtom);
+  const [, setCount2ByJotai] = useAtom(count2Atom);
   const [countValue] = useAtom(getCountValue);
+  const [count2Value] = useAtom(getCount2Value);
   const [doubleCountValue] = useAtom(getDoubleCountValue);
+  const [sumCountValue] = useAtom(getSumCountValue);
   const [countAtomWithStorage, setCountAtomWithStorage] = useAtom(
     countAtomWithStorageAtom
   );
 
   return (
-    <div className="flex gap-x-8 min-h-screen text-zinc-200 bg-base-100 p-6">
-      <div className="my-2">
-        <div className="mb-2">Component State</div>
-        <button
-          type="button"
-          onClick={() => setCount((c) => c + 1)}
-          className="btn btn-primary mr-2"
-        >
-          count up
-        </button>
-        <button
-          type="button"
-          onClick={() => setCount((c) => c - 1)}
-          className="btn btn-primary"
-        >
-          count down
-        </button>
-        <div className="mt-2 text-lg">count is: {count}</div>
+    <div className="min-h-screen text-zinc-200 bg-base-100 p-6">
+      <div className="flex gap-x-8">
+        <div className="my-2">
+          <div className="mb-2">Component State</div>
+          <button
+            type="button"
+            onClick={() => setCount((c) => c + 1)}
+            className="btn btn-primary mr-2"
+          >
+            count up
+          </button>
+          <button
+            type="button"
+            onClick={() => setCount((c) => c - 1)}
+            className="btn btn-primary"
+          >
+            count down
+          </button>
+          <div className="mt-2 text-lg">count is: {count}</div>
+        </div>
+        <div className="my-2">
+          <div className="mb-2">Redux State</div>
+          <button
+            type="button"
+            onClick={() => dispatch(increment(countByRecoil))}
+            className="btn btn-warning mr-2"
+          >
+            count up
+          </button>
+          <button
+            type="button"
+            onClick={() => dispatch(decrement(countByRecoil))}
+            className="btn btn-warning"
+          >
+            count down
+          </button>
+          <div className="mt-2 text-lg">count is: {countByRedux}</div>
+        </div>
+        <div className="my-2">
+          <div className="mb-2">Context State</div>
+          <button
+            type="button"
+            onClick={() => setCountByContext(countByContext + 1)}
+            className="btn btn-error mr-2"
+          >
+            count up
+          </button>
+          <button
+            type="button"
+            onClick={() => setCountByContext(countByContext - 1)}
+            className="btn btn-error"
+          >
+            count down
+          </button>
+          <div className="mt-2 text-lg">count is: {countByContext}</div>
+        </div>
       </div>
-      <div className="my-2">
-        <div className="mb-2">Recoil State</div>
-        <button
-          type="button"
-          onClick={() => countSetState({ count: countByRecoil + 1 })}
-          className="btn btn-success mr-2"
-        >
-          count up
-        </button>
-        <button
-          type="button"
-          onClick={() => countSetState({ count: countByRecoil - 1 })}
-          className="btn btn-success"
-        >
-          count down
-        </button>
-        <div className="mt-2 text-lg">count is: {countByRecoil}</div>
-
-        <button
-          type="button"
-          onClick={() => count2SetState({ count: count2ByRecoil + 1 })}
-          className="btn btn-success mr-2"
-        >
-          count up
-        </button>
-        <button
-          type="button"
-          onClick={() => count2SetState({ count: count2ByRecoil - 1 })}
-          className="btn btn-success"
-        >
-          count down
-        </button>
-        <div className="mt-2 text-lg">count2 is: {count2ByRecoil}</div>
-        <div className="mt-2 text-lg">sum count is: {sumCount}</div>
+      <div className="flex gap-x-8">
+        <div className="my-2">
+          <div className="mb-2">Recoil State</div>
+          <div className="mb-2">
+            <button
+              type="button"
+              onClick={() => countSetState({ count: countByRecoil + 1 })}
+              className="btn btn-success mr-2"
+            >
+              count up
+            </button>
+            <button
+              type="button"
+              onClick={() => countSetState({ count: countByRecoil - 1 })}
+              className="btn btn-success"
+            >
+              count down
+            </button>
+          </div>
+          <div>
+            <button
+              type="button"
+              onClick={() => count2SetState({ count: count2ByRecoil + 1 })}
+              className="btn btn-success mr-2"
+            >
+              count2 up
+            </button>
+            <button
+              type="button"
+              onClick={() => count2SetState({ count: count2ByRecoil - 1 })}
+              className="btn btn-success"
+            >
+              count2 down
+            </button>
+          </div>
+          <div className="mt-2 text-lg">count is: {countByRecoil}</div>
+          <div className="mt-2 text-lg">count2 is: {count2ByRecoil}</div>
+          <div className="mt-2 text-lg">sum count is: {sumCount}</div>
+        </div>
       </div>
-      <div className="my-2">
-        <div className="mb-2">Redux State</div>
-        <button
-          type="button"
-          onClick={() => dispatch(increment(countByRecoil))}
-          className="btn btn-warning mr-2"
-        >
-          count up
-        </button>
-        <button
-          type="button"
-          onClick={() => dispatch(decrement(countByRecoil))}
-          className="btn btn-warning"
-        >
-          count down
-        </button>
-        <div className="mt-2 text-lg">count is: {countByRedux}</div>
-      </div>
-      <div className="my-2">
-        <div className="mb-2">Context State</div>
-        <button
-          type="button"
-          onClick={() => setCountByContext(countByContext + 1)}
-          className="btn btn-error mr-2"
-        >
-          count up
-        </button>
-        <button
-          type="button"
-          onClick={() => setCountByContext(countByContext - 1)}
-          className="btn btn-error"
-        >
-          count down
-        </button>
-        <div className="mt-2 text-lg">count is: {countByContext}</div>
-      </div>
-      <div className="my-2">
-        <div className="mb-2">Jotai State</div>
-        <button
-          type="button"
-          onClick={() =>
-            setCountByJotai((p) => ({
-              count: p.count + 1,
-            }))
-          }
-          className="btn bg-sky-600 hover:bg-sky-600 mr-2"
-        >
-          count up
-        </button>
-        <button
-          type="button"
-          onClick={() =>
-            setCountByJotai((p) => ({
-              count: p.count - 1,
-            }))
-          }
-          className="btn bg-sky-600 hover:bg-sky-600"
-        >
-          count down
-        </button>
-        <div className="mt-2 text-lg">count is: {countByJotai.count}</div>
-        <div className="mt-1 text-lg">count is: {countValue}</div>
-        <div className="mt-1 text-lg">double count is: {doubleCountValue}</div>
-      </div>
-      <div className="my-2">
-        <div className="mb-2">Jotai State with LocalStorage</div>
-        <button
-          type="button"
-          onClick={() => setCountAtomWithStorage((p) => p + 1)}
-          className="btn bg-violet-600 hover:bg-violet-600 mr-2"
-        >
-          count up
-        </button>
-        <button
-          type="button"
-          onClick={() => setCountAtomWithStorage((p) => p - 1)}
-          className="btn bg-violet-600 hover:bg-violet-600"
-        >
-          count down
-        </button>
-        <div className="mt-2 text-lg">count is: {countAtomWithStorage}</div>
+      <div className="flex gap-x-8">
+        <div className="my-2">
+          <div className="mb-2">Jotai State</div>
+          <div className="mb-2">
+            <button
+              type="button"
+              onClick={() =>
+                setCountByJotai((p) => ({
+                  count: p.count + 1,
+                }))
+              }
+              className="btn bg-sky-600 hover:bg-sky-600 mr-2"
+            >
+              count up
+            </button>
+            <button
+              type="button"
+              onClick={() =>
+                setCountByJotai((p) => ({
+                  count: p.count - 1,
+                }))
+              }
+              className="btn bg-sky-600 hover:bg-sky-600"
+            >
+              count down
+            </button>
+          </div>
+          <div>
+            <button
+              type="button"
+              onClick={() =>
+                setCount2ByJotai((p) => ({
+                  count: p.count + 1,
+                }))
+              }
+              className="btn bg-sky-600 hover:bg-sky-600 mr-2"
+            >
+              count2 up
+            </button>
+            <button
+              type="button"
+              onClick={() =>
+                setCount2ByJotai((p) => ({
+                  count: p.count - 1,
+                }))
+              }
+              className="btn bg-sky-600 hover:bg-sky-600"
+            >
+              count2 down
+            </button>
+          </div>
+          <div className="mt-1 text-lg">count is: {countValue}</div>
+          <div className="mt-1 text-lg">count2 is: {count2Value}</div>
+          <div className="mt-1 text-lg">
+            double count is: {doubleCountValue}
+          </div>
+          <div className="mt-1 text-lg">sum count is: {sumCountValue}</div>
+        </div>
+        <div className="my-2">
+          <div className="mb-2">Jotai State with LocalStorage</div>
+          <button
+            type="button"
+            onClick={() => setCountAtomWithStorage((p) => p + 1)}
+            className="btn bg-violet-600 hover:bg-violet-600 mr-2"
+          >
+            count up
+          </button>
+          <button
+            type="button"
+            onClick={() => setCountAtomWithStorage((p) => p - 1)}
+            className="btn bg-violet-600 hover:bg-violet-600"
+          >
+            count down
+          </button>
+          <div className="mt-2 text-lg">count is: {countAtomWithStorage}</div>
+        </div>
       </div>
     </div>
   );
--- src/stores/jotai/atom.ts
+++ src/stores/jotai/atom.ts
@@ -2,8 +2,13 @@ import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
 export const countAtom = atom({ count: 0 });
+export const count2Atom = atom({ count: 0 });
 export const getCountValue = atom((get) => get(countAtom).count);
+export const getCount2Value = atom((get) => get(count2Atom).count);
 export const getDoubleCountValue = atom((get) => get(countAtom).count * 2);
+export const getSumCountValue = atom(
+  (get) => get(countAtom).count + get(count2Atom).count
+);
 
 export const countAtomWithStorageAtom = atomWithStorage(
   'countAtomWithStorage',
